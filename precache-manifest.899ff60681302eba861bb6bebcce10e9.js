929e7ddcbb6ca2246fa8bdc2b059bd07e3152643
jasminegmp <jasminekgump@gmail.com>
2019-06-29 23:04:46 -0700

    Updates

---
+++ precache-manifest.899ff60681302eba861bb6bebcce10e9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07d8bd3ed6b157513e728994dc86b4e1",
+    "url": "/slack_clone/index.html"
+  },
+  {
+    "revision": "09d6b0de8f52dda5821a",
+    "url": "/slack_clone/static/js/2.432d116c.chunk.js"
+  },
+  {
+    "revision": "c1a9c6fce5dd678801fb",
+    "url": "/slack_clone/static/js/main.27870d95.chunk.js"
+  },
+  {
+    "revision": "0dac2c20b37925c8acff",
+    "url": "/slack_clone/static/js/runtime~main.0e9ab88e.js"
+  }
+]);

bfa5c8d5c7c5829019118c90a47f5711255fcba9
jasminegmp <jasminekgump@gmail.com>
2019-07-01 17:49:36 -0700

    Able to route path from Login and Register to homepage

--- src/index.js
+++ src/index.js
@@ -4,17 +4,41 @@ import App from './component/App';
 import Login from './component/auth/Login';
 import Register from './component/auth/Register';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { withRouter } from 'react-router';
 import 'semantic-ui-css/semantic.min.css';
+import firebase from './Firebase';
 
-const Root = () => (
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import {composeWithDevTools} from 'redux-devtools-extension';
+
+
+class Root extends React.Component{
+    componentDidMount(){
+        firebase.auth().onAuthStateChanged(user => {
+            if (user){
+                //console.log(user);
+                this.props.history.push('/');
+            }
+        })
+    }
+    render(){
+        return (
+            <Switch>
+                <Route exact path = {`${process.env.PUBLIC_URL}/`} component = {App} />
+                <Route path = {`${process.env.PUBLIC_URL}/login`}component = {Login} />
+                <Route path = {`${process.env.PUBLIC_URL}/register`} component = {Register} />
+            </Switch>
+        )
+    }
+}
+
+const RootWithAuth = withRouter(Root);
+
+ReactDOM.render(
     <Router>
-        <Switch>
-            <Route exact path = {`${process.env.PUBLIC_URL}/`} component = {App} />
-            <Route path = {`${process.env.PUBLIC_URL}/login`}component = {Login} />
-            <Route path = {`${process.env.PUBLIC_URL}/register`} component = {Register} />
-        </Switch>
+        <RootWithAuth />
     </Router>
-);
-
-ReactDOM.render(<Root />, document.getElementById('root'));
+, 
+document.getElementById('root'));
 

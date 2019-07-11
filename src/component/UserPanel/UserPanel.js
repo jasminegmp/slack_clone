ca0a6bf56bb5dcb277a1e1480d51c9d922b5047a
jasminegmp <jasminekgump@gmail.com>
2019-07-10 17:00:37 -0700

    Routing based on whether user is logged in or off

---
+++ src/component/UserPanel/UserPanel.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import {Grid, Header, Icon, Dropdown} from 'semantic-ui-react';
+import firebase from '../../Firebase'
+
+class UserPanel extends React.Component{
+
+    dropdownOptions = () => [
+        {
+            key: "user",
+            text: <span>Signed in as  <strong>User</strong></span>,
+            disabled: true // not clickable
+        },
+        {
+            key: "avatar",
+            text: <span>Change Avatar</span>
+        },
+        {
+            key: "signout",
+            text: <span onClick = {this.handleSignout} >Sign Out</span>
+        }
+
+    ]
+
+    handleSignout = () => {
+        firebase
+            .auth()
+            .signOut()
+            .then(() => console.log('logged off!'));
+
+    }
+
+    render(){
+        return(
+            <Grid>
+                <Grid.Column>
+                    <Grid.Row style = {{padding: '1.2em', margin: 0}}>
+                        {/* Main App Header */}
+                        <Header inverted floated ="left" as ="h2">
+                            <Icon name = "code" />
+                            <Header.Content>GumpChat</Header.Content>
+                        </Header>
+                    </Grid.Row>
+
+                    {/* User Drop Down with Sign Out*/}
+                    <Header style = {{padding: '0.25em'}} as = 'h4' inverted>
+                        <Dropdown trigger = {
+                            <span>User</span>
+                        } options = {this.dropdownOptions()} />
+                    </Header>
+                </Grid.Column>
+            </Grid>
+        )
+    }
+}
+
+export default UserPanel;
